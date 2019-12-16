e429c5ef79383877de28a40c96c4db5e03f53e5b
Leo G <leoko4433@gmail.com>
2019-12-16 06:54:55 +0100

    Basic spring boot steup for rest-api
    Packages for JWT Support might still be missing

---
+++ backend/src/test/java/io/dcbn/backend/DcbnApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dcbn.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcbnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
