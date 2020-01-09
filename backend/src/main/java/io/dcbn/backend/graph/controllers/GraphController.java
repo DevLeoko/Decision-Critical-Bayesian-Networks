96004e1d1f18de0072a1a0d5d47de836c883347f
Fabio Mayer <fabio.mayer@student.kit.edu>
2020-01-10 00:08:34 +0100

    Added GraphController and GraphRepository.

---
+++ backend/src/main/java/io/dcbn/backend/graph/controllers/GraphController.java
@@ -0,0 +1,70 @@
+package io.dcbn.backend.graph.controllers;
+
+import io.dcbn.backend.graph.Graph;
+import io.dcbn.backend.graph.repositories.GraphRepository;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@PreAuthorize("hasRole('MODERATOR')")
+public class GraphController {
+
+  private final GraphRepository repository;
+
+  @Autowired
+  public GraphController(GraphRepository repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping("/graphs")
+  public Iterable<Graph> getGraphs(@RequestParam boolean withStructure) {
+    return StreamSupport.stream(repository.findAll().spliterator(), false).peek(graph -> {
+      if (!withStructure) {
+        graph.setNodes(Collections.emptyList());
+      }
+    }).collect(Collectors.toList());
+  }
+
+  @GetMapping("/graphs/{id}")
+  public Graph getGraphById(@PathVariable long id) {
+    return repository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+  }
+
+  @PostMapping("/graphs")
+  public void createGraph(@Valid @RequestBody Graph graph) {
+    repository.save(graph);
+  }
+
+  @DeleteMapping("/graphs/{id}")
+  public void deleteById(@PathVariable long id) {
+    if (!repository.existsById(id)) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    repository.deleteById(id);
+  }
+
+  @PutMapping("/graphs/{id}")
+  public void updateGraphById(@PathVariable long id, @Valid @RequestBody Graph graph) {
+    Graph oldGraph = repository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    oldGraph.setNodes(graph.getNodes());
+    oldGraph.setName(graph.getName());
+    oldGraph.setTimeSlices(graph.getTimeSlices());
+    repository.save(oldGraph);
+  }
+}
