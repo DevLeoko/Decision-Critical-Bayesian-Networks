27290795da554a1285732c955cc9ac8d889920f9
Daniel Vollmer <daniel.vollmer98@gmail.com>
2020-01-17 19:27:05 +0100

    Fixed activemq Producer.

--- backend/src/main/java/io/dcbn/backend/core/activemq/Producer.java
+++ backend/src/main/java/io/dcbn/backend/core/activemq/Producer.java
@@ -1,5 +1,6 @@
 package io.dcbn.backend.core.activemq;
 
+import io.dcbn.backend.maritimedatamodel.JsonMapper;
 import io.dcbn.backend.maritimedatamodel.Network;
 import io.dcbn.backend.maritimedatamodel.Outcome;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -8,14 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class Producer {
 
-    @SendTo("network.queue")
-    public static Network sendNetwork(Network network) {
-        return network;
-    }
-
     @SendTo("outcome.queue")
-    public static Outcome sendOutcome(Outcome outcome) {
-        return outcome;
+    public static String sendOutcome(Outcome outcome) {
+        return JsonMapper.toJson(outcome);
     }
 
     @SendTo("error.queue")
