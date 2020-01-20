20296eff6d3fedcca0b10542cd4f8646e5df4784
Fabio Mayer <fabio.mayer@student.kit.edu>
2020-01-20 15:54:30 +0100

    Server response no longer includes stacktrace.

--- backend/src/main/java/io/dcbn/backend/evidenceFormula/services/exceptions/EvaluationException.java
+++ backend/src/main/java/io/dcbn/backend/evidenceFormula/services/exceptions/EvaluationException.java
@@ -1,4 +1,7 @@
 package io.dcbn.backend.evidenceFormula.services.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"message", "localizedMessage", "cause", "suppressed", "stackTrace"})
 public abstract class EvaluationException extends RuntimeException {
 }
