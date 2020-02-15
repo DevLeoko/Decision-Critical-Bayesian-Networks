139ea451f74927199620d583f950e3835d5e1e44
Leo G <leoko4433@gmail.com>
2020-02-15 22:52:33 +0100

    Fixed import issue

--- frontend/src/components/graph/CptContainer.vue
+++ frontend/src/components/graph/CptContainer.vue
@@ -21,7 +21,7 @@
 <script lang="ts">
 import Vue from "vue";
 import ProbTable from "@/components/graph/ProbTable.vue";
-import { dcbn } from "../../utils/graph";
+import { dcbn } from "@/utils/graph/graph";
 
 export default Vue.extend({
   components: {
