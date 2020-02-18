f83849d9bda56a1e87db4cb3565418db88c1150a
Johann <johann.bonneau2000@gmail.com>
2020-02-18 22:07:14 +0100

    Enhenced cpt code

--- frontend/src/components/graph/properties/ProbTable.vue
+++ frontend/src/components/graph/properties/ProbTable.vue
@@ -1,53 +1,10 @@
 <template>
   <div>
-    <!-- time 0 -->
-    <v-container v-if="time0" fill-height class="mx-3">
-      <v-row v-for="i in node.timeZeroDependency.parents.length" :key="i">
-        <v-col
-          cols="3"
-          style="outline-width: 2px;
-  outline-style: solid;
-  outline-color: black;"
-        >
-          {{ node.timeZeroDependency.parents[i - 1] }}
-        </v-col>
-        <v-col
-          style="outline-width: 2px;
-  outline-style: solid;
-  outline-color: black;"
-          v-for="j in Math.pow(2, i)"
-          :key="j"
-        >
-          <v-col>
-            {{ node.stateType.states[(j + 1) % 2] }}
-          </v-col>
-        </v-col>
-      </v-row>
+    <v-container fill-height class="mx-3">
       <v-row
-        v-for="i in node.stateType.states.length"
-        :key="i"
-        style="outline-width: 2px;
-  outline-style: solid;
-  outline-color: black;"
-      >
-        <v-col cols="3"> {{ node.stateType.states[i - 1] }}</v-col>
-        <v-col
-          v-for="k in Math.pow(2, node.timeZeroDependency.parents.length)"
-          :key="k"
-          style="outline-width: 2px;
-  outline-style: solid;
-  outline-color: black;"
-        >
-          <v-text-field dense outlined solo></v-text-field
-        ></v-col>
-      </v-row>
-    </v-container>
-    <!-- time t -->
-    <v-container v-else fill-height class="mx-3">
-      <v-row
-        v-for="k in node.timeTDependency.parents.length +
-          node.timeTDependency.parentsTm1.length"
-        :key="k"
+        v-for="parents in dependency.parents.length +
+          dependency.parentsTm1.length"
+        :key="parents"
       >
         <v-col
           cols="3"
@@ -55,15 +12,11 @@
   outline-style: solid;
   outline-color: black;"
         >
-          <div v-if="k - 1 < node.timeTDependency.parents.length">
-            {{ node.timeTDependency.parents[k - 1] }}
+          <div v-if="parents - 1 < dependency.parents.length">
+            {{ dependency.parents[parents - 1] }}
           </div>
           <div v-else>
-            {{
-              node.timeTDependency.parentsTm1[
-                k - 1 - node.timeTDependency.parents.length
-              ]
-            }}
+            {{ dependency.parentsTm1[parents - 1 - dependency.parents.length] }}
             (T-1)
           </div>
         </v-col>
@@ -71,29 +24,28 @@
           style="outline-width: 2px;
   outline-style: solid;
   outline-color: black;"
-          v-for="j in Math.pow(2, k)"
-          :key="j"
+          v-for="parentStates in Math.pow(2, parents)"
+          :key="parentStates"
         >
           <v-col>
-            {{ node.stateType.states[(j + 1) % 2] }}
+            {{ stateType.states[(parentStates + 1) % 2] }}
           </v-col>
         </v-col>
       </v-row>
       <v-row
-        v-for="i in node.stateType.states.length"
-        :key="i"
+        v-for="nodeStates in stateType.states.length"
+        :key="nodeStates"
         style="outline-width: 2px;
   outline-style: solid;
   outline-color: black;"
       >
-        <v-col cols="3"> {{ node.stateType.states[i - 1] }}</v-col>
+        <v-col cols="3"> {{ stateType.states[nodeStates - 1] }}</v-col>
         <v-col
-          v-for="l in Math.pow(
+          v-for="inputFields in Math.pow(
             2,
-            node.timeZeroDependency.parents.length +
-              node.timeTDependency.parentsTm1.length
+            dependency.parents.length + dependency.parentsTm1.length
           )"
-          :key="l"
+          :key="inputFields"
           style="outline-width: 2px;
   outline-style: solid;
   outline-color: black;"
@@ -111,11 +63,8 @@ import { dcbn } from "@/utils/graph/graph";
 
 export default Vue.extend({
   props: {
-    node: Object as () => dcbn.Node,
-    time0: Boolean
-  },
-  data() {
-    return {};
+    dependency: Object as () => dcbn.TimeZeroDependency | dcbn.TimeTDependency,
+    stateType: Object as () => dcbn.StateType
   }
 });
 </script>
